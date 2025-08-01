55ed0c6d198bed57707ef58e318b301336f8265a
Jason Zhong <47954501+ZhongDev@users.noreply.github.com>
2025-08-01 20:09:41 +1000

    Add unit conversion Utility

---
+++ src/util-modules/UnitConverter/meta.ts
@@ -0,0 +1,13 @@
+import { ModuleMeta } from '../../types/module';
+
+export const title = 'Unit Converter';
+export const route = '/unit-converter';
+export const icon = '⚖️';
+
+const meta: ModuleMeta = {
+  title,
+  route,
+  icon,
+};
+
+export default meta;
